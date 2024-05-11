661006d02f67711c281e72341762a6476a481387
Adriána Bohunická <adabo4@gmail.com>
2024-05-11 22:01:27 +0200

    refactored navbar component

--- src/Home.js
+++ src/Home.js
@@ -1,75 +1,19 @@
-import React, { useState } from 'react'
-import "./Home.css"
-import About from './About'
-import Skills from './Skills'
-import Projects from './Projects'
-import Contact from './Contact'
-import { NavLink } from 'react-router-dom'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { FaTimes } from 'react-icons/fa';
-
-
+import React from "react";
+import "./Home.css";
+import About from "./About";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Navbar from "./NAVBAR/Navbar";
 
 export default function Home() {
-
-    const scrollTo = (x, y) => {
-        window.scrollTo({
-            top: x,
-            left: y,
-            behavior: "smooth",
-        });
-    }
-
-    function scroolSkills() {
-
-        var scrollSkills = document.getElementById("skills").getBoundingClientRect().top;
-        window.scrollTo({ top: scrollSkills, behavior: 'smooth' });
-
-    }
-    function scroolProjects() {
-        var scrollProjects = document.getElementById("projects").getBoundingClientRect().top;
-        window.scrollTo({ top: scrollProjects, behavior: 'smooth' });
-    }
-    function scroolContact() {
-        var scrollContact = document.getElementById("contact").getBoundingClientRect().top;
-        window.scrollTo({ top: scrollContact, behavior: 'smooth' });
-    }
-
-
-
-    const [show, setShow] = useState(false);
-
-    const toggleMenu = () => {
-        setShow(!show);
-    };
-
-
-
-    return (
-
-        <>
-            <nav className='navbar'>
-                <h2>
-                    <GiHamburgerMenu className='hamburger-icon' onClick={toggleMenu} />
-                </h2>
-                <ul className={`nav-menu ${show ? "show" : "hide"}`}>
-                    <NavLink to="#about" onClick={() => { scrollTo(100, 100); toggleMenu() }}><li>About</li></NavLink>
-                    <NavLink to="#skills" onClick={() => { scroolSkills(); toggleMenu() }}><li>Skills</li></NavLink>
-
-                    <NavLink to="#projects" onClick={() => { scroolProjects(); toggleMenu() }}><li>Projects</li></NavLink>
-                    <NavLink to="#contact" onClick={() => { scroolContact(); toggleMenu() }}><li>Contact</li></NavLink>
-
-                </ul>
-
-                {show && <FaTimes className='nav-close-btn' onClick={toggleMenu}></FaTimes>}
-            </nav>
-
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-        </>
-
-
-    )
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
 }
---
+++ src/NAVBAR/Navbar.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
+import "./navbar.css";
+
+export default function Navbar() {
+  const scrollTo = (x, y) => {
+    window.scrollTo({
+      top: x,
+      left: y,
+      behavior: "smooth",
+    });
+  };
+
+  function scroolSkills() {
+    var scrollSkills = document
+      .getElementById("skills")
+      .getBoundingClientRect().top;
+    window.scrollTo({ top: scrollSkills, behavior: "smooth" });
+  }
+  function scroolProjects() {
+    var scrollProjects = document
+      .getElementById("projects")
+      .getBoundingClientRect().top;
+    window.scrollTo({ top: scrollProjects, behavior: "smooth" });
+  }
+  function scroolContact() {
+    var scrollContact = document
+      .getElementById("contact")
+      .getBoundingClientRect().top;
+    window.scrollTo({ top: scrollContact, behavior: "smooth" });
+  }
+
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <h2>
+          <GiHamburgerMenu className="hamburger-icon" onClick={toggleMenu} />
+        </h2>
+        <ul className={`nav-menu ${show ? "show" : "hide"}`}>
+          <NavLink
+            to="#about"
+            onClick={() => {
+              scrollTo(100, 100);
+              toggleMenu();
+            }}
+          >
+            <li>About</li>
+          </NavLink>
+          <NavLink
+            to="#skills"
+            onClick={() => {
+              scroolSkills();
+              toggleMenu();
+            }}
+          >
+            <li>Skills</li>
+          </NavLink>
+
+          <NavLink
+            to="#projects"
+            onClick={() => {
+              scroolProjects();
+              toggleMenu();
+            }}
+          >
+            <li>Projects</li>
+          </NavLink>
+          <NavLink
+            to="#contact"
+            onClick={() => {
+              scroolContact();
+              toggleMenu();
+            }}
+          >
+            <li>Contact</li>
+          </NavLink>
+        </ul>
+
+        {show && (
+          <FaTimes className="nav-close-btn" onClick={toggleMenu}></FaTimes>
+        )}
+      </nav>
+    </>
+  );
+}
