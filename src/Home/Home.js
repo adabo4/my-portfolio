40b1f4cccf6a953a42370c3122fbedaafd191d24
Adriána Bohunická <adabo4@gmail.com>
2024-05-11 22:36:23 +0200

    refactored Home component

---
+++ src/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+import About from "../AboutUs/About";
+import Skills from "../Skills/Skills";
+import Projects from "../Projects/Projects";
+import Contact from "../Contact/Contact";
+import Navbar from "../NAVBAR/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
